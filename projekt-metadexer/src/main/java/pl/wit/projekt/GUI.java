943e528cc1dd6b4420cc8b30f4a1ea3a75f23e71
Jan Konarski <37694742+Nexonus@users.noreply.github.com>
2024-05-06 23:58:24 +0200

    Updated GUI + Copying mechanism
    - Made changes to the GUI's look added JTextPanel with Scroll
    - Modified copying, so that no duplicates are made

--- projekt-metadexer/src/main/java/pl/wit/projekt/GUI.java
+++ projekt-metadexer/src/main/java/pl/wit/projekt/GUI.java
@@ -3,6 +3,7 @@ package pl.wit.projekt;
 /***
  * @author Jan Konarski
  * GUI - Metadexer
+ * Implementacja interfejsu graficznego programu do indeksowania i sortowania plików JPG na bazie metadanych EXIF
  */
 
 import java.awt.*;
@@ -10,81 +11,206 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 import org.apache.commons.imaging.ImagingException;
 
 public class GUI extends JFrame implements ActionListener {
-	private JPanel contentPane;
-	private JFileChooser chooseFile = new JFileChooser(new File("..\\samples"));
-	private JTextField tbInputPath = new JTextField("",35);
-	private String folderPath = new String("");
 	
-	/// Create interface
+	/// Create TextFields
+	private JTextField tbInputPath = new JTextField("",60);
+	private JTextField tbOutputPath = new JTextField("",60);
+	
+	/// Create fileChoosers
+	private JFileChooser fcInputChooser = new JFileChooser(new File("..\\"));	// default start directory
+	private JFileChooser fcOutputChooser = new JFileChooser(new File("..\\"));
+	// Create Buttons
+	private JButton btnInputFolder = new JButton("Choose Input Folder");
+	private JButton btnOutputFolder = new JButton("Choose Output Folder");
+	private JButton btnStart = new JButton("Start");
+	
+	// Create Panels
+	private JPanel pnContentPane = new JPanel(new BorderLayout());
+	private	JPanel pnFolderPane = new JPanel(new BorderLayout());
+	private JPanel pnInputPane = new JPanel(new GridLayout(1,2));
+	private JPanel pnOutputPane = new JPanel(new GridLayout(1,2));
+	private JPanel pnStartPane = new JPanel(new BorderLayout());
+	private JPanel pnStartPaneSplit = new JPanel(new GridLayout(1,2));
+	
+	/// Output log components
+	private JTextPane tpScrollPane = new JTextPane();
+	private JScrollPane spScrollPane = new JScrollPane(tpScrollPane);
+	
+	private JLabel lbOutputLogLabel = new JLabel("Output log:");
+	/***
+	 * 
+	 * @throws ImagingException
+	 * @throws IOException
+	 */
 	public GUI() throws ImagingException, IOException {
-		super("Metadexer");
-				
+		super("METADEXER");
+
+		
+		tpScrollPane.setBackground(Color.BLACK);
+		spScrollPane.setBorder(new EmptyBorder(5,5,5,5));
+		
+		/// Create a new 750 x 450 px window for GUI, default pane is pnContentPane:
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100,100,650,300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(10,10,10,10));
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+		setBounds(100,100,750,450);
 		
-		setContentPane(contentPane);
-	
-		JButton btnChooseInputFolder = new JButton("Open");
-		btnChooseInputFolder.addActionListener(this);
+		pnContentPane.setBorder(new EmptyBorder(5,5,5,5));
+		pnStartPane.setBorder(new EmptyBorder(2,2,2,2));
+		pnFolderPane.setBorder(new EmptyBorder(2,2,2,2));
 		
-		JButton btnStart = new JButton("Start");
-		Metadata metadata = new Metadata();
-		btnStart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent actionEvent) {
+		lbOutputLogLabel.setBorder(new EmptyBorder(5,5,5,5));
+		
+		//pnContentPane.setLayout(new FlowLayout(FlowLayout.CENTER,15,5)); // create flowLayout (flexBox)
+		setContentPane(pnContentPane);
+				
+		/// Set debug colors for each panel
+		pnContentPane.setBackground(Color.WHITE); 
+		pnFolderPane.setBackground(Color.LIGHT_GRAY); 
+		pnStartPane.setBackground(Color.LIGHT_GRAY);
+		pnStartPaneSplit.setBackground(Color.WHITE);
+		
+		/// Add ActionListeners to buttons
+		btnInputFolder.addActionListener(this);
+		btnOutputFolder.addActionListener(this);
+		btnStart.addActionListener(this);
+			
+		/// Choose Directories with fileChoosers
+		fcInputChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fcOutputChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		
+		/// Add components to the panels
+		pnInputPane.add(tbInputPath, BorderLayout.EAST);
+		pnInputPane.add(btnInputFolder,BorderLayout.CENTER);
+		pnFolderPane.add(pnInputPane,BorderLayout.NORTH);
+		
+		pnOutputPane.add(tbOutputPath,BorderLayout.EAST);
+		pnOutputPane.add(btnOutputFolder,BorderLayout.CENTER);
+		pnFolderPane.add(pnOutputPane,BorderLayout.SOUTH);
+		
+		// Add panels to the ContentPane, mostly visual stuff
+		pnStartPaneSplit.add(lbOutputLogLabel,BorderLayout.WEST); // borderlayout.west property non-necessary for any GridLayout if set as so
+		pnStartPaneSplit.add(btnStart,BorderLayout.EAST);		  // mandatory for BorderLayout
+		
+		pnStartPane.add(pnStartPaneSplit,BorderLayout.NORTH);
+		pnContentPane.add(pnStartPane,BorderLayout.CENTER);
+		
+		pnContentPane.add(pnFolderPane,BorderLayout.NORTH);
+		
+		
+		// Set TextArea from OutputLog to non-editable
+		//tpScrollPane.setEditable(false);
+		tpScrollPane.setEditable(false);
+		
+		// Add ScrollPane to the GUI components.
+		pnStartPane.add(spScrollPane, BorderLayout.CENTER);
+		
+		
+		/** Important! **/
+		/// Make sure app is visible, off by default! Super important!
+		setVisible(true);
+	}
+	/***
+	 * Action Performed : Check for which button is pressed and read DialogResult to perform an action.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		Object source = ae.getSource();
+		String strInputPath = tbInputPath.getText();
+		String strOutputPath = tbOutputPath.getText();
+		int result;
+		
+		if (source == btnInputFolder) {
+
+			result = fcInputChooser.showOpenDialog(this);
+			if (result == fcInputChooser.APPROVE_OPTION) {
+				strInputPath = fcInputChooser.getSelectedFile().getAbsolutePath().toString();
+				tbInputPath.setText(strInputPath);
+			}else {
+				strInputPath = "";
+				tbInputPath.setText(strInputPath);
+			}
+		}else if (source == btnOutputFolder) {
+			result = fcOutputChooser.showOpenDialog(this);
+			if (result == fcOutputChooser.APPROVE_OPTION) {
+				strOutputPath = fcOutputChooser.getSelectedFile().getAbsolutePath();
+				tbOutputPath.setText(strOutputPath);
+			}else {
+				strOutputPath = "";
+				tbOutputPath.setText(strOutputPath);
+			}
+		}else if (source == btnStart) {
+			
+			boolean inputValid = false;
+			boolean outputValid = false;
+			
+			tpScrollPane.setText(""); // 
+			if (checkValidFolderPath(tbInputPath.getText()) && tbInputPath.getText() != null && !tbInputPath.getText().isEmpty()) {
+				inputValid = true;
+			}else {
+				tbInputPath.setText("");
+				appendToPane(tpScrollPane,"[ERROR] - Invalid input path: ".concat(strInputPath).concat("\n"), Color.RED);
+			}
+			if (checkValidFolderPath(tbOutputPath.getText()) && tbOutputPath.getText() != null && !tbOutputPath.getText().isEmpty()) {
+				outputValid = true;
+			}else {
+				tbOutputPath.setText("");
+				appendToPane(tpScrollPane,"[ERROR] - Invalid output path: ".concat(strOutputPath).concat("\n"), Color.RED);
+			}
+			if (inputValid && outputValid) {
+				Metadata metadata = new Metadata();
+				metadata.setStrOutputDirectoryPath(strOutputPath.concat("\\"));
 				try {
-					metadata.DiscoverImages("..\\samples");
-				} catch (IOException e) {
-					e.printStackTrace();
+					metadata.DiscoverImages(strInputPath);
+					
+					if (metadata.getIndexedImageList().size()>0) {
+						appendToPane(tpScrollPane,"[SUCCESS] - Copied Files: ".concat("\n").concat("\n"), Color.GREEN);
+					}else {
+						appendToPane(tpScrollPane,"[WARNING] - No unique files to copy found.".concat("\n").concat("\n"), Color.ORANGE);
+					}
+					for (String imageName : metadata.getIndexedImageList()) {
+						appendToPane(tpScrollPane,imageName.concat("\n"), Color.WHITE);
+					}
+					
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
 			}
-		});
-		
-		JPanel filePane = new JPanel();
-		JPanel startPane = new JPanel();
-		
-		contentPane.setBackground(Color.WHITE);
-		filePane.setBackground(Color.LIGHT_GRAY);
-		
-		startPane.setBackground(Color.RED);
-		
-		JLabel lbInputLabel = new JLabel();
-		lbInputLabel.setText("Path to input images folder");
-		lbInputLabel.setForeground(Color.WHITE);
-		
-		tbInputPath.setEditable(false);
-		chooseFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		
-		filePane.add(lbInputLabel);
-		filePane.add(tbInputPath);
-		filePane.add(btnChooseInputFolder);
-		
-		startPane.add(btnStart);
-		
-		contentPane.add(filePane);
-		contentPane.add(startPane);
-		
-		
-		setVisible(true);	/// By default a window is hidden!
-	}
-	public void actionPerformed(ActionEvent e) {
-		int result = chooseFile.showOpenDialog(this);
-		if(result == chooseFile.APPROVE_OPTION) {
-			folderPath = chooseFile.getSelectedFile().getAbsolutePath();
-			tbInputPath.setText(folderPath);
-		}else if (result == chooseFile.CANCEL_OPTION){
-			folderPath = "";
-			tbInputPath.setText(folderPath);
 		}
 	}
+	/**
+	 * Check if Path leads to a valid existing file (Directory)
+	 * @param path
+	 * @return
+	 */
+	public boolean checkValidFolderPath(String path) {
+		if (Files.exists(Paths.get(path))) {
+			return true;
+		}
+		return false;
+	}
+	private void appendToPane(JTextPane tpScrollPane, String message, Color c) {
+		tpScrollPane.setEditable(true);
+		StyleContext sc = StyleContext.getDefaultStyleContext();
+		AttributeSet as = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, c);
+		
+		int length = tpScrollPane.getDocument().getLength();
+		tpScrollPane.setCaretPosition(length);
+		tpScrollPane.setCharacterAttributes(as, false);
+		tpScrollPane.replaceSelection(message);
+		tpScrollPane.setEditable(false);
+	}
 }
+
 
--- projekt-metadexer/src/main/java/pl/wit/projekt/Metadata.java
+++ projekt-metadexer/src/main/java/pl/wit/projekt/Metadata.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -26,18 +28,30 @@ import org.apache.commons.io.FilenameUtils;
 
 public class Metadata {
 	
-	public String getFileExtension(String filename) {
-		return FilenameUtils.getExtension(filename);
-	}
+	private String strOutputDirectoryPath=null;
+	private String strOutputImagePath=null;
+	private List<String> indexedImageList = new ArrayList<String>();
+	private List<Long> fileSizeList = new ArrayList<Long>();
+	
 	/***
 	 * 
 	 * @param jpegMetadata
 	 * @param tagInfo
 	 * @return
 	 */
+	/// Extract selected TiffField (EXIF data) from image file
 	private static String getDateMetadata(final JpegImageMetadata jpegMetadata, final TagInfo tagInfo) {
 		final TiffField field = jpegMetadata.findExifValueWithExactMatch(tagInfo);
 		return (field.getValueDescription().toString().substring(1, field.getValueDescription().length()-10).replace(':', '.'));		
+	}
+	/**
+	 * 
+	 * @param filename
+	 * @return
+	 */
+	/// Get file extension (could do it with regex though..)
+	public String getFileExtension(String filename) {
+		return FilenameUtils.getExtension(filename);
 	}
 	/***
 	 * 
@@ -45,6 +59,7 @@ public class Metadata {
 	 * @throws ImagingException
 	 * @throws IOException
 	 */
+	/// Scan for JPG or JPEG images in the parent directory and sub-directories - Directory is INPUT path
 	public void DiscoverImages(String directory) throws ImagingException, IOException{
 		
 		Path rootDirectory = Paths.get(directory);
@@ -56,36 +71,50 @@ public class Metadata {
 					.filter(Files::isRegularFile)
 					.collect(Collectors.toSet());
 			// Use Set to select only unique dates!
-			
+			// This is important to make sure that we don't get duplicate output folders for sorting.
 			
 			for (Path path : pathSet) {
+				
 				if (getFileExtension(path.toString()).equals("jpg") || getFileExtension(path.toString()).equals("jpeg")) {
 					File file = new File(path.toString());
-					String outputPath = new String();
-					String outputImagePath = new String();
 					
+					// Get metadata
 					final ImageMetadata metadata = Imaging.getMetadata(file);
 					final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
 					
-					if (jpegMetadata != null) {
-						Integer index = 0;
-						outputPath = "..\\output\\".concat(getDateMetadata(jpegMetadata, ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL)).concat("\\");
-						if (!Files.exists(Paths.get(outputPath))) {
-							Files.createDirectories(Paths.get(outputPath));
-							//System.out.println("Creating Directory: ".concat(outputPath));
-							/// Debug msg
-						}
-						while (Files.exists(Paths.get(outputImagePath))) {
-							++index;
-							outputImagePath = outputPath.concat(index.toString().concat(".").concat(getFileExtension(path.toString())));
-						}
-						System.out.println(outputImagePath);
-						Files.copy(path, Paths.get(outputImagePath));
+					// MAKE SURE, that the image actually contains TIFF fields, this is true for natural JPG files (done with a camera), but
+					// this won't apply for artificially generated images, we're skipping these.
 					
-						//Files.copy(path, previousFilePath, StandardCopyOption.REPLACE_EXISTING);
+					if (jpegMetadata != null) {
+						Integer imageIndex = 1;
 						
-						/// TO DO: Replace existing files!
+						String localOutputDirectory = (getStrOutputDirectoryPath().concat(getDateMetadata(jpegMetadata, ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL)).concat("\\"));
+						String localOutputImage = localOutputDirectory.concat(imageIndex.toString()).concat(".").concat(getFileExtension(path.toString()));
 						
+						if (!Files.exists(Paths.get(localOutputDirectory))) {
+							Files.createDirectories(Paths.get(localOutputDirectory));
+						}
+						while (Files.exists(Paths.get(localOutputImage))) {
+							++imageIndex;
+							localOutputImage = localOutputDirectory.concat(imageIndex.toString()).concat(".").concat(getFileExtension(path.toString()));
+						}
+						
+						/***
+						 * Copy files, only if these weren't added yet. Check if there's any file with exact same size in bytes (length)
+						 */
+						File currentDirectory = new File(localOutputDirectory);
+						File[] directoryFiles = currentDirectory.listFiles();
+						
+						if (directoryFiles != null) {
+							for (File child : directoryFiles) {
+								fileSizeList.add(child.length());
+							}
+						}
+						if (!fileSizeList.contains(Files.size(path))) {
+							indexedImageList.add(localOutputImage);
+							Files.copy(path, Paths.get(localOutputImage));
+						}
+						System.out.println(localOutputImage);
 					}
 				}
 			}
@@ -93,4 +122,18 @@ public class Metadata {
 			e.printStackTrace();
 		}
 	}
+	public String getStrOutputDirectoryPath() {
+		return strOutputDirectoryPath;
+	}
+	public String getStrOutputImagePath() {
+		return strOutputImagePath;
+	}
+	public List<String> getIndexedImageList() {
+		return indexedImageList;
+	}
+	public void setStrOutputDirectoryPath(String strOutputDirectoryPath) {
+		this.strOutputDirectoryPath = strOutputDirectoryPath;
+	}
+	
 }
+
